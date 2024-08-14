d38d3f80dfb444dfd2921b56662b2b400908953d
TIWIMIN <tiwimin@gmail.com>
2024-08-13 21:44:28 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const myLibrary = []; 
+
+function Book(title, author, pages, read) {
+    this.title = title; 
+    this.author = author; 
+    this.pages = pages; 
+    this.read = read;
+    this.info = function() {
+        if (read) {
+            return title + " by " + author + ", " + pages + " pages, read."
+        }
+        else {
+            return title + " by " + author + ", " + pages + " pages, not read yet."
+        }
+    }
+}
+
+function addBookToLibrary() {
+
+}
